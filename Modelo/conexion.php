56b1437fe50c4f920f2896d8348cbede83b7acaa
2021030063 <152040336+2021030063@users.noreply.github.com>
2023-12-05 16:52:17 -0600

    Update conexion.php
    Back to usual connect.php

--- Modelo/conexion.php
+++ Modelo/conexion.php
@@ -1,8 +1,8 @@
 <?php
 class Conexion{
         static public function conectar(){
-            //$enlace = new PDO("mysql:host=localhost;dbname=cristo7","root","1976197720032006");
-            $enlace = new PDO("mysql:host=localhost;dbname=7_lemi_mvc","root","alpine");
+            $enlace = new PDO("mysql:host=localhost;dbname=cristo7","root","1976197720032006");
+            //$enlace = new PDO("mysql:host=localhost;dbname=7_lemi_mvc","root","alpine");
             $enlace->exec("set names utf8");
             return $enlace;
         }
